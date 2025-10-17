5e0e7a668bbda0165dc218c3efca3a0dec40a189
mikey <mikeyrocoo@gmail.com>
2025-10-17 19:55:49 +0300

    feat: add game logic
    - Add score variables; computerScore and humanScore.
    
    Computer choice logic
    - Modified getComputerChoice() function to properly return a valid
      answer ('rock', 'paper' or 'scissors').
    
    Game logic
    - Create playRound() function, taking in humanChoice and computerChoice.
    - Added comparisons to playRound() function, comparing humanChoice
      and computerChoice to conclude the winner and increment score of
      winner by 1.

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
-const getComputerChoice = () => Math.floor(Math.random() * 3) + 1;
-const getHumanChoice = () => prompt("Rock, Paper or Scissors?").toLowerCase();
+"use strict";
+
+let humanScore = 0, computerScore = 0;
+
+const getHumanChoice = () => prompt("Rock, Paper or Scissors?");
+
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  return (randomNumber === 1) ? "rock" :
+  (randomNumber === 2) ? "paper" :
+  "scissors";
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice == computerChoice) {
+    alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
+
+  } else if (humanChoice == "rock") {
+    if (computerChoice == "paper") {
+      alert("Computer chose paper..\nYou lose! Paper beats rock!");
+      computerScore++;
+    } else if (computerChoice == "scissors") {
+      alert("Computer chose scissors..\n You win! Rock beats scissors!");
+      humanScore++;
+    }
+
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      alert("Computer chose rock..\nYou win! Paper beats rock!");
+      humanScore++;
+    } else if (computerChoice == "scissors") {
+      alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
+      computerScore++;
+    }
+    
+  } else if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      alert("Computer chose rock..\nYou lose! Rock beats scissors!");
+      computerScore++;
+    } else if (computerChoice == "paper") {
+      alert("Computer chose paper..\nYou win! Scissors beat paper!");
+      humanScore++;
+    }
+  }
+}
