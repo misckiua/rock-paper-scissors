d6800e5682619504f1bef250524172bd0cca35b9
mikey <mikeyrocoo@gmail.com>
2025-10-17 19:00:42 +0300

    fix: getComputerChoice() returning only 1 and 2

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const getComputerChoice = () => Math.floor((Math.random() * 2) + 1);
+const getComputerChoice = () => Math.floor(Math.random() * 3) + 1;
 const getHumanChoice = () => prompt("Rock, Paper or Scissors?").toLowerCase();
