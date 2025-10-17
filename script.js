3f4d053c71bd626d3c7abf149feffc6da273079a
mikey <mikeyrocoo@gmail.com>
2025-10-17 20:33:57 +0300

    feat: add main game loop
    Small Changes
    - Remove line between getHumanChoice() and getComputerChoice().
    
    Main game loop
    - Create playGame() function.
    - Move playRound() function and score variables declarations into
      playGame() function.
    - Create constant variable for number of rounds game is played
      (NUMBER_OF_ROUNDS).
    - Add loop to repeat playRound() until certain number of rounds (as
      declared in NUMBER_OF_ROUNDS variable).
    - Add alert to show final game scores.

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 "use strict";
 
-let humanScore = 0, computerScore = 0;
+const NUMBER_OF_ROUNDS = 5;
 
 const getHumanChoice = () => prompt("Rock, Paper or Scissors?");
-
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -12,37 +11,52 @@ function getComputerChoice() {
   "scissors";
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
+function playGame() {
+  let humanScore = 0, computerScore = 0;
 
-  if (humanChoice == computerChoice) {
-    alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
 
-  } else if (humanChoice == "rock") {
-    if (computerChoice == "paper") {
-      alert("Computer chose paper..\nYou lose! Paper beats rock!");
-      computerScore++;
-    } else if (computerChoice == "scissors") {
-      alert("Computer chose scissors..\n You win! Rock beats scissors!");
-      humanScore++;
-    }
+    if (humanChoice == computerChoice) {
+      alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
 
-  } else if (humanChoice == "paper") {
-    if (computerChoice == "rock") {
-      alert("Computer chose rock..\nYou win! Paper beats rock!");
-      humanScore++;
-    } else if (computerChoice == "scissors") {
-      alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
-      computerScore++;
-    }
-    
-  } else if (humanChoice == "scissors") {
-    if (computerChoice == "rock") {
-      alert("Computer chose rock..\nYou lose! Rock beats scissors!");
-      computerScore++;
-    } else if (computerChoice == "paper") {
-      alert("Computer chose paper..\nYou win! Scissors beat paper!");
-      humanScore++;
+    } else if (humanChoice == "rock") {
+      if (computerChoice == "paper") {
+        alert("Computer chose paper..\nYou lose! Paper beats rock!");
+        computerScore++;
+      } else if (computerChoice == "scissors") {
+        alert("Computer chose scissors..\n You win! Rock beats scissors!");
+        humanScore++;
+      }
+
+    } else if (humanChoice == "paper") {
+      if (computerChoice == "rock") {
+        alert("Computer chose rock..\nYou win! Paper beats rock!");
+        humanScore++;
+      } else if (computerChoice == "scissors") {
+        alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
+        computerScore++;
+      }
+
+    } else if (humanChoice == "scissors") {
+      if (computerChoice == "rock") {
+        alert("Computer chose rock..\nYou lose! Rock beats scissors!");
+        computerScore++;
+      } else if (computerChoice == "paper") {
+        alert("Computer chose paper..\nYou win! Scissors beat paper!");
+        humanScore++;
+      }
     }
   }
+  
+  for (let rounds = 1; rounds <= NUMBER_OF_ROUNDS; rounds++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  alert(`Final Scores\nComputer: ${computerScore}\nYou: ${humanScore}`)
 }
+
+playGame()
