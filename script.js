ba2bfad624493a22b0a62f67d164870d4d1f57f7
mikey <mikeyrocoo@gmail.com>
2025-10-23 18:02:40 +0300

    add: header styling to index.html
    Added:
    - Header styling.
    - Main and footer background color, and border color and radius.
    
    Fixed:
    - Font import using Roboto instead of Lexend.
    
    Modified:
    - Winner info text in index.html.
    - Commented entire JS file temporarily.

--- script.js
+++ script.js
@@ -1,62 +1,62 @@
-"use strict";
+// "use strict";
 
-const NUMBER_OF_ROUNDS = 5;
+// const NUMBER_OF_ROUNDS = 5;
 
-const getHumanChoice = () => prompt("Rock, Paper or Scissors?");
-function getComputerChoice() {
-  let randomNumber = Math.floor(Math.random() * 3) + 1;
+// const getHumanChoice = prompt("Rock, Paper or Scissors?");
+// function getComputerChoice() {
+//   let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-  return (randomNumber === 1) ? "rock" :
-  (randomNumber === 2) ? "paper" :
-  "scissors";
-}
+//   return (randomNumber === 1) ? "rock" :
+//   (randomNumber === 2) ? "paper" :
+//   "scissors";
+// }
 
-function playGame() {
-  let humanScore = 0, computerScore = 0;
+// function playGame() {
+//   let humanScore = 0, computerScore = 0;
 
-  function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
+//   function playRound(humanChoice, computerChoice) {
+//     humanChoice = humanChoice.toLowerCase();
 
-    if (humanChoice == computerChoice) {
-      alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
+//     if (humanChoice == computerChoice) {
+//       alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
 
-    } else if (humanChoice == "rock") {
-      if (computerChoice == "paper") {
-        alert("Computer chose paper..\nYou lose! Paper beats rock!");
-        computerScore++;
-      } else if (computerChoice == "scissors") {
-        alert("Computer chose scissors..\n You win! Rock beats scissors!");
-        humanScore++;
-      }
+//     } else if (humanChoice == "rock") {
+//       if (computerChoice == "paper") {
+//         alert("Computer chose paper..\nYou lose! Paper beats rock!");
+//         computerScore++;
+//       } else if (computerChoice == "scissors") {
+//         alert("Computer chose scissors..\n You win! Rock beats scissors!");
+//         humanScore++;
+//       }
 
-    } else if (humanChoice == "paper") {
-      if (computerChoice == "rock") {
-        alert("Computer chose rock..\nYou win! Paper beats rock!");
-        humanScore++;
-      } else if (computerChoice == "scissors") {
-        alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
-        computerScore++;
-      }
+//     } else if (humanChoice == "paper") {
+//       if (computerChoice == "rock") {
+//         alert("Computer chose rock..\nYou win! Paper beats rock!");
+//         humanScore++;
+//       } else if (computerChoice == "scissors") {
+//         alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
+//         computerScore++;
+//       }
 
-    } else if (humanChoice == "scissors") {
-      if (computerChoice == "rock") {
-        alert("Computer chose rock..\nYou lose! Rock beats scissors!");
-        computerScore++;
-      } else if (computerChoice == "paper") {
-        alert("Computer chose paper..\nYou win! Scissors beat paper!");
-        humanScore++;
-      }
-    }
-  }
+//     } else if (humanChoice == "scissors") {
+//       if (computerChoice == "rock") {
+//         alert("Computer chose rock..\nYou lose! Rock beats scissors!");
+//         computerScore++;
+//       } else if (computerChoice == "paper") {
+//         alert("Computer chose paper..\nYou win! Scissors beat paper!");
+//         humanScore++;
+//       }
+//     }
+//   }
   
-  for (let rounds = 1; rounds <= NUMBER_OF_ROUNDS; rounds++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+//   for (let rounds = 1; rounds <= NUMBER_OF_ROUNDS; rounds++) {
+//     const humanSelection = getHumanChoice();
+//     const computerSelection = getComputerChoice();
 
-    playRound(humanSelection, computerSelection);
-  }
+//     playRound(humanSelection, computerSelection);
+//   }
 
-  alert(`Final Scores\nComputer: ${computerScore}\nYou: ${humanScore}`)
-}
+//   alert(`Final Scores\nComputer: ${computerScore}\nYou: ${humanScore}`)
+// }
 
-playGame()
+// playGame()
