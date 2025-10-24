696b3772090233ca061bc430f4864bd7240019ce
mikey <mikeyrocoo@gmail.com>
2025-10-24 11:27:32 +0300

    add: modal for endgame
    When MAX_POINTS is met for any player, the modal will display
    showing the overall winner of the game.
    - Created modal container, with child modal.
    - Added h1, paragraph, and button.
    - Added basic styling to modal and hid it (opacity: 0;) and changed
      z-index (z-index: -1;).
    
    - After the playRound() function runs, it will check and see if
      any of the players have hit MAX_POINTS, and if so, will show the
      modal.
    - Added an event listener to the button in the modal that hides it.

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 "use strict";
 
-const NUMBER_OF_ROUNDS = 5;
+const MAX_POINTS = 5;
+const ROCK_EMOJI = "✊";
+const PAPER_EMOJI = "✋";
+const SCISSORS_EMOJI = "✌️";
 
 const buttons = document.querySelector("#buttons");
+const humanChoiceDisplay = document.querySelector("#human-choice-display");
+const computerChoiceDisplay = document.querySelector("#computer-choice-display");
+const humanScoreDisplay = document.querySelector("#human-score");
+const computerScoreDisplay = document.querySelector("#computer-score");
+const winnerOfRoundDisplay = document.querySelector("#winner-info");
+const reasonOfWinDisplay = document.querySelector("#winner-msg");
+
+const modal = document.querySelector("#modal-container");
+const retryModalButton = modal.querySelector("#retry-modal-button");
+
+reasonOfWinDisplay.textContent = `First to ${MAX_POINTS} wins the game!`;
+
+let humanScore = 0, computerScore = 0;
+let isGameOver = false;
+
+retryModalButton.addEventListener("click", () => {
+  modal.classList.remove("active");
+});
 
 buttons.addEventListener("click", (event) => {
-  const humanChoice = event.target.id;
-  playRound(humanChoice, getComputerChoice());
+  if (!isGameOver) {
+    const buttonPressed = event.target.id;
+    // Just a temporary fix for the font size..
+    computerChoiceDisplay.style.fontSize = "100px";
+    humanChoiceDisplay.style.fontSize = "100px";
+    playRound(buttonPressed, getComputerChoice());
+  }
 });
+
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -17,40 +44,89 @@ function getComputerChoice() {
   "scissors";
 }
 
-let humanScore = 0, computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
 
   if (humanChoice == computerChoice) {
-    alert(`You chose: ${humanChoice}, Computer chose: ${computerChoice}.\nTie!`);
+    humanChoiceDisplay.textContent = (humanChoice === "rock") ? ROCK_EMOJI :
+    (humanChoice === "paper") ? PAPER_EMOJI :
+    SCISSORS_EMOJI;
+
+    computerChoiceDisplay.textContent = (computerChoice === "rock") ? ROCK_EMOJI :
+    (computerChoice === "paper") ? PAPER_EMOJI :
+    SCISSORS_EMOJI;
+
+    winnerOfRoundDisplay.textContent = "Tie!";
+    reasonOfWinDisplay.textContent = "No one won!";
 
   } else if (humanChoice == "rock") {
+    humanChoiceDisplay.textContent = ROCK_EMOJI;
+
     if (computerChoice == "paper") {
-      alert("Computer chose paper..\nYou lose! Paper beats rock!");
-      computerScore++;
+
+      computerChoiceDisplay.textContent = PAPER_EMOJI;
+      winnerOfRoundDisplay.textContent = "You lose!";
+      reasonOfWinDisplay.textContent = "Paper eats rock!";
+
+      computerScoreDisplay.textContent = `Computer: ${++computerScore}`;
+
     } else if (computerChoice == "scissors") {
-      alert("Computer chose scissors..\n You win! Rock beats scissors!");
-      humanScore++;
+
+      computerChoiceDisplay.textContent = SCISSORS_EMOJI;
+      winnerOfRoundDisplay.textContent = "You win!";
+      reasonOfWinDisplay.textContent = "Rock destroys scissors!";
+
+      humanScoreDisplay.textContent = `You: ${++humanScore}`;
+
     }
 
   } else if (humanChoice == "paper") {
+    humanChoiceDisplay.textContent = PAPER_EMOJI;
+
     if (computerChoice == "rock") {
-      alert("Computer chose rock..\nYou win! Paper beats rock!");
-      humanScore++;
+
+      computerChoiceDisplay.textContent = ROCK_EMOJI;
+      winnerOfRoundDisplay.textContent = "You win!";
+      reasonOfWinDisplay.textContent = "Paper eats rock!";
+
+      humanScoreDisplay.textContent = `You: ${++humanScore}`
+
     } else if (computerChoice == "scissors") {
-      alert("Computer chose scissors..\nYou lose! Scissors beat paper!");
-      computerScore++;
+
+      computerChoiceDisplay.textContent = SCISSORS_EMOJI;
+      winnerOfRoundDisplay.textContent = "You lose!";
+      reasonOfWinDisplay.textContent = "Scissors slice paper!";
+
+      computerScoreDisplay.textContent = `Computer: ${++computerScore}`;
+
     }
 
   } else if (humanChoice == "scissors") {
+    humanChoiceDisplay.textContent = SCISSORS_EMOJI;
+
     if (computerChoice == "rock") {
-      alert("Computer chose rock..\nYou lose! Rock beats scissors!");
-      computerScore++;
+
+      computerChoiceDisplay.textContent = ROCK_EMOJI;
+      winnerOfRoundDisplay.textContent = "You lose!";
+      reasonOfWinDisplay.textContent = "Rock destroys scissors!";
+
+      computerScoreDisplay.textContent = `Computer: ${++computerScore}`;
+
     } else if (computerChoice == "paper") {
-      alert("Computer chose paper..\nYou win! Scissors beat paper!");
-      humanScore++;
+
+      computerChoiceDisplay.textContent = PAPER_EMOJI;
+      winnerOfRoundDisplay.textContent = "You win!";
+      reasonOfWinDisplay.textContent = "Scissors slice paper!";
+
+      humanScoreDisplay.textContent = `You: ${++humanScore}`;
+
     }
+  }
+
+  if (humanScore >= MAX_POINTS || computerScore >= MAX_POINTS) {
+    isGameOver = true;
+    modal.classList.add("active");
   }
 }
 
